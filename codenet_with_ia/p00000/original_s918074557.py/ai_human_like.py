c793903e66106f78200e11d968220bca0df25745
Your Name <you@example.com>
2025-05-14 21:36:48 +0200

    added dataset processor and batch manager

--- codenet_with_ia/p00000/original_s918074557.py/ai_human_like.py
+++
@@ -1,5 +0,0 @@
-for i in range(1, 11):
-    for j in range(1, 11):
-          # Affiche le résultat de la multiplication
-        print(str(i) + 'x' + str(j) + '=' + str(i*j))
-    print("")  # Ajoute un saut de ligne entre chaque table de multiplication
