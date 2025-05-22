145518cd5ef7e0bc1c18838f4e2dcefbfd57e542
LeDuc34 <37620753+LeDuc34@users.noreply.github.com>
2025-05-22 16:00:23 +0200

    push push push

---
+++ data/archives/scripts/var_p00000_s918074557.py_style_procedural.py
@@ -0,0 +1,9 @@
+i = 1
+while i < 10:
+    j = 1
+    while j < 10:
+        resultat = i * j
+        texte = str(i) + "x" + str(j) + "=" + str(resultat)
+        print(texte)
+        j += 1
+    i += 1
